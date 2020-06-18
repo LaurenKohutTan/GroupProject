afdf3a1535202c8470bd985c3695d63cdb47740b
Davidson9000 <66446914+Davidson9000@users.noreply.github.com>
2020-06-18 18:48:47 -0500

    DuncanHello

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,7 @@ SpringApplication.run(DemoApplication.class, args);
 }
 
 @GetMapping("/hello")
-public String hello(@RequestParam(value = "name", defaultValue = "Sucker (cause this all blows)") String name) {
+public String hello(@RequestParam(value = "name", defaultValue = "Sucker (cause this is annoying)") String name) {
 return String.format("Hello %s!", name);
 }
 }
